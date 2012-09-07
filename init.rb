4407481845ef59f7553d49e2cb64b5e7cdfb2f8b
Alec Chen <alec@cpan.org>
2012-09-07 22:07:26 +0800

    first commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_pygments do
+  name 'Redmine Pygments plugin'
+  author 'Alec Chen'
+  description 'This is a syntax highlight plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://about.me/alecchen'
+end
